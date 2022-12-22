aa3203bdf9490aa33c49a1499eeb54daa83222dd
arijitsarkar199 <119077109+arijitsarkar199@users.noreply.github.com>
2022-12-22 10:02:14 -0800

    landing page update

--- Execution/Js/LandingPage.js
+++ Execution/Js/LandingPage.js
@@ -11,3 +11,19 @@ function linkclick(){
     }
 }
 // LandingPage End
+// password start
+const toggle=document.querySelector(".toggle");
+const input=document.getElementById("password");
+
+toggle.addEventListener("click",()=>{
+    if(input.type==="password"){
+        input.type="text";
+        toggle.classList.replace("fa-eye-slash","fa-eye")
+    }
+    else{
+        input.type="password";
+        toggle.classList.replace("fa-eye","fa-eye-slash")
+
+    }
+})
+// password end
