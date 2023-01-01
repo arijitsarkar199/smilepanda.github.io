b2462fccf488097a63032e158c698c454e5f3ece
arijitsarkar199 <119077109+arijitsarkar199@users.noreply.github.com>
2023-01-01 03:21:39 -0800

    footer update testing

--- Execution/Js/Script.js
+++ Execution/Js/Script.js
@@ -1,45 +1,127 @@
 // Header Start
-let toggleopen=document.getElementById("navbar");
-let fabars=document.getElementById("fabars");
-let myshoppingCart=document.getElementById("myshoppingCart");
-const opennav=()=>{
-    toggleopen.className="openbar";
-    fabars.style.display="none";
-    myshoppingCart.style.marginTop="-200px"
+let toggleopen = document.getElementById("navbar");
+let fabars = document.getElementById("fabars");
+let myshoppingCart = document.getElementById("myshoppingCart");
+const opennav = () => {
+    toggleopen.className = "openbar";
+    fabars.style.display = "none";
+    myshoppingCart.style.marginTop = "-200px"
 }
-const closenav=()=>{
-    toggleopen.className="closebar";
-    fabars.style.display="block";
-    myshoppingCart.style.marginTop="0px"
+const closenav = () => {
+    toggleopen.className = "closebar";
+    fabars.style.display = "block";
+    myshoppingCart.style.marginTop = "0px"
 }
+
 // cart-shopping start
-let count=0;
-let counter=document.getElementById("counter");
-let addAnimationa= document.getElementsByClassName("addAnimation")
-for(let x of addAnimationa){
-    x.addEventListener("click",event=>{
-     const c1=counter.classList;
-     const c="animatiosAdd";
-     count++;
-    
-     counter.innerText=count;
-     c1.remove(c,c1.contains(c));
-     setTimeout(()=>
-     counter.classList.add("animatiosAdd")
-     ,1)
+let count = 0;
+let counter = document.getElementById("counter");
+let addAnimationa = document.getElementsByClassName("addAnimation")
+for (let x of addAnimationa) {
+    x.addEventListener("click", event => {
+        const c1 = counter.classList;
+        const c = "animatiosAdd";
+        count++;
+
+        counter.innerText = count;
+        c1.remove(c, c1.contains(c));
+        setTimeout(() =>
+            counter.classList.add("animatiosAdd")
+            , 1)
     })
 }
 // cart-shopping end
 // Header End
 // DeliveryAdd Start
-let deliveryTextspan=document.getElementsByClassName("deliveryTextspan");
-const deliveryEvent=()=>{
-    for(let x of deliveryTextspan){
-        x.style.color="#f46b27";
+let deliveryTextspan = document.getElementsByClassName("deliveryTextspan");
+const deliveryEvent = () => {
+    for (let x of deliveryTextspan) {
+        x.style.color = "#f46b27";
     }
 }
 // DeliveryAdd End
+// Best Seller Start
+let myhearts = document.getElementById("myheart");
+let heartClick = () => {
+    if (myhearts.style.opacity == "1") {
 
+        myhearts.style.opacity = "0";
+    } else {
+        myhearts.style.opacity = "1";
+
+    }
+}
+let myhearts2 = document.getElementById("myheart2");
+let heartClick2 = () => {
+    if (myhearts2.style.opacity == "1") {
+
+        myhearts2.style.opacity = "0";
+    } else {
+        myhearts2.style.opacity = "1";
+
+    }
+}
+let myhearts3 = document.getElementById("myheart3");
+let heartClick3 = () => {
+    if (myhearts3.style.opacity == "1") {
+
+        myhearts3.style.opacity = "0";
+    } else {
+        myhearts3.style.opacity = "1";
+
+    }
+}
+let myhearts4 = document.getElementById("myheart4");
+let heartClick4 = () => {
+    if (myhearts4.style.opacity == "1") {
+
+        myhearts4.style.opacity = "0";
+    } else {
+        myhearts4.style.opacity = "1";
+
+    }
+}
+let myhearts5 = document.getElementById("myheart5");
+let heartClick5 = () => {
+    if (myhearts.style.opacity == "1") {
+
+        myhearts5.style.opacity = "0";
+    } else {
+        myhearts5.style.opacity = "1";
+
+    }
+}
+let myhearts6 = document.getElementById("myheart6");
+let heartClick6 = () => {
+    if (myhearts6.style.opacity == "1") {
+
+        myhearts6.style.opacity = "0";
+    } else {
+        myhearts6.style.opacity = "1";
+
+    }
+}
+let myhearts7 = document.getElementById("myheart7");
+let heartClick7 = () => {
+    if (myhearts.style.opacity == "1") {
+
+        myhearts7.style.opacity = "0";
+    } else {
+        myhearts7.style.opacity = "1";
+
+    }
+}
+let myhearts8 = document.getElementById("myheart8");
+let heartClick8 = () => {
+    if (myhearts8.style.opacity == "1") {
+
+        myhearts8.style.opacity = "0";
+    } else {
+        myhearts8.style.opacity = "1";
+
+    }
+}
+// Best Seller End
 // Blogs Start
 function carousel() {
     let carouselSlider = document.querySelector(".carousel__slider");
